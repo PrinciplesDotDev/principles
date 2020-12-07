df825ebff2171588f3538c526c95a52485161675
Adam Craven <git@adamcraven.me>
2020-12-07 14:08:07 +0000

    validate

--- sh.sh
+++ sh.sh
@@ -1,9 +1,9 @@
-for changed_file in core-logic-separate-from-framework.md; do
+for changed_file in anothem.md compute-properties-when-possible.md core-logic-separate-from-framework.md documentation-close-to-code.md invisible-visible.md; do
     if [[ $changed_file == *.md ]] && [[ $changed_file != README.md ]];
     then
         echo "Do something with this ${changed_file}."
         output="$(cat $changed_file)"
-        # echo "$output"
+        echo "$output"
         curlOutput=$(curl --location -w '\n\nStatusCode:%{http_code}'  --request PUT 'https://t1h8ftxrq4.execute-api.eu-west-2.amazonaws.com/default/validator-prod' --header 'Content-Type: text/plain' --data-raw "${output}")
         statusCode=${curlOutput:(-3)}
 
