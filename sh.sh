0e97c2c4c4f187900872a31ee36183b39b0e66b0
Adam Craven <git@adamcraven.me>
2020-12-07 14:12:26 +0000

    try again

--- sh.sh
+++ sh.sh
@@ -1,20 +1,15 @@
 for changed_file in anothem.md compute-properties-when-possible.md core-logic-separate-from-framework.md documentation-close-to-code.md invisible-visible.md; do
-    if [[ $changed_file == *.md ]] && [[ $changed_file != README.md ]];
-    then
+    if [[ $changed_file == *.md ]] && [[ $changed_file != README.md ]]; then
         echo "Do something with this ${changed_file}."
         output="$(cat $changed_file)"
-        echo "$output"
         curlOutput=$(curl --location -w '\n\nStatusCode:%{http_code}'  --request PUT 'https://t1h8ftxrq4.execute-api.eu-west-2.amazonaws.com/default/validator-prod' --header 'Content-Type: text/plain' --data-raw "${output}")
         statusCode=${curlOutput:(-3)}
 
         if [[ $statusCode == "200" ]]; then
-            echo "SUCCESS!"
-            exit 0
+            echo "VALID: ${changed_file}"
         else
-            echo "FAILED"
-            echo ${curlOutput%StatusCode*}
+            echo "FAILED Validation: ${changed_file}: ${curlOutput%StatusCode*}"
             exit 1
-            #echo ${curlOutput%StatusCode*}
         fi
     fi
 done
