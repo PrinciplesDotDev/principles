a4a8a2516cfcccab9c5938b3e592fce756bc1566
Adam Craven <git@adamcraven.me>
2020-12-07 11:40:35 +0000

    fix script

--- sh.sh
+++ sh.sh
@@ -5,13 +5,13 @@ for changed_file in core-logic-separate-from-framework.md; do
         output="$(cat $changed_file)"
         # echo "$output"
         curlOutput=$(curl --location -w '\n\nStatusCode:%{http_code}'  --request PUT 'https://t1h8ftxrq4.execute-api.eu-west-2.amazonaws.com/default/validator-prod' --header 'Content-Type: text/plain' --data-raw "${output}")
+        statusCode=${curlOutput:(-3)}
 
-        if ${curlOutput:(-3)} == 200:
-        then
+        if [[ $statusCode == "200" ]]; then
             echo "SUCCESS!"
         else
             echo "FAILED"
-            echo ${curlOutput%StatusCode*}
+            #echo ${curlOutput%StatusCode*}
         fi
     fi
 done
